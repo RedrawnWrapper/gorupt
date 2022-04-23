cfcb2cc6ee17ca45f42cbe82c6ff6e0cc066a7b2
florian9721 <totheftothea@gmail.com>
2022-04-23 22:27:22 +1000

    ok

---
+++ static/js/0xpvTKsBbG/main/index.js.gq.js
@@ -0,0 +1,9 @@
+// Main Table
+var main = {};
+
+// All the variables / functions
+main.cssid = "0vRhCBFqXs";
+main.jsid = "0xpvTKsBbG";
+
+// Logs the current static version
+console.log("Current Static: " + main.cssid + ", " + main.jsid)
