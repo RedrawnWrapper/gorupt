4e74e9c8b6e3fae44c11c4018397036841e7ea4a
florzaa <101799513+florzaa@users.noreply.github.com>
2022-05-02 16:30:15 +1000

    Delete static directory

--- static/js/0xpvTKsBbG/main/index.js.gq.js
+++
@@ -1,9 +0,0 @@
-// Main Table
-var main = {};
-
-// All the variables / functions
-main.cssid = "0vRhCBFqXs";
-main.jsid = "0xpvTKsBbG";
-
-// Logs the current static version
-console.log("Current Static: " + main.cssid + ", " + main.jsid)
